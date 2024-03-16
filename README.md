6dd770bbd0626945a885fea2483e100a5ab6f224
amugisha <amugisha200@gmail.com>
2024-03-16 15:30:22 -0700

    corrected code

--- README.md
+++ README.md
@@ -2,4 +2,3 @@
 A movies database implemented in SQL, with a Python CLI. 
 
 Remember to run `pip install -r requirements.txt` before running the code.
-In your SQL server, run 'SET GLOBAL local_infile=1' to allow importing CSV files.
--- grant-permissions.sql
+++ grant-permissions.sql
@@ -1,6 +1,6 @@
 CREATE USER 'admin1'@'localhost' IDENTIFIED BY 'admin1pw';
 CREATE USER 'client1'@'localhost' IDENTIFIED BY 'client1pw';
 -- Can add more users or refine permissions
-GRANT ALL PRIVILEGES ON moviesdb.* TO 'appadmin'@'localhost';
-GRANT SELECT ON moviesdb.* TO 'appclient'@'localhost';
+GRANT ALL PRIVILEGES ON moviesdb.* TO 'admin1'@'localhost';
+GRANT SELECT ON moviesdb.* TO 'client1'@'localhost';
 FLUSH PRIVILEGES;
